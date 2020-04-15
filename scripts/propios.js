1d723e611b7a347c4669e31fa6c95d6ffcc2e68b
Noelia Méndez <noelia_davinson@hotmail.com>
2020-04-14 23:19:22 -0500

    Error con funcion onchange

---
+++ scripts/propios.js
@@ -0,0 +1,9 @@
+
+        function ShowSelected(){
+        var numero = document.getElementById("tipo").value;
+        
+        if (numero == "1"){
+             console.log('1');
+        }else{
+            console.log('0');
+        }
